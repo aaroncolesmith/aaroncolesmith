f869bf1aee2942c091d0ea44416248271a40ab9c
Aaron Smith <aaroncolesmith@gmail.com>
2021-01-17 14:10:39 -0800

    adding data apps

--- about.py
+++ about.py
@@ -17,6 +17,7 @@ def app():
     with st.beta_expander("Bovada"):
         st.write("""
         Visualize betting odds scraped over time
+
         A very interesting set of data to see how implied probability of an event changes over time. Take a look at the 2020 Presidential Election, it is a pretty interesting visualization.
         """)
         st.image('./images/bovada.gif',use_column_width=True)
@@ -24,6 +25,7 @@ def app():
     with st.beta_expander("COVID-Viz"):
         st.write("""
         Visualize COVID cases & deaths over time, broken down by country
+
         My goal was to not only allow someone to view COVID cases and deaths over time, but also introduce some metrics that would give an indication whether a given country was on the rise or falling.
         Note: this one utlizes lots of data, so it may take some time to load
         """)
@@ -32,6 +34,7 @@ def app():
     with st.beta_expander("CLSTR"):
         st.write("""
         Automatically cluster datasets -- choose from a couple presets or load a CSV to load your own data
+
         I've been very interested in unsupervised learning where you can just throw an algorithm a set of data and see the results. That was the intention with CLSTR which will allow you to upload your own data and see the results. This could be used for customer segmentation, recommendation engines, among many other applications.
         """)
         st.image('./images/clstr.gif',use_column_width=True)
@@ -39,6 +42,7 @@ def app():
     with st.beta_expander("NBA Clusters"):
         st.write("""
         See how different NBA players/careers cluster based on similar stats
+
         A more specific clustering example, I wanted to see if I could take a data set with a number of different features and see the results. NBA Reference collects a number of different statistics ranging from scoring to rebounding to defensive metrics. It is very interesting to see how certain plays have similar statistical careers.
         """)
         st.image('./images/nba_clusters.gif',use_column_width=True)
@@ -46,6 +50,7 @@ def app():
     with st.beta_expander("NBA Redraftables"):
         st.write("""
         How would NBA teams redraft based on a player's statistical career? Based on [The Ringer's Podcast Series](https://www.theringer.com/nba/2020/4/1/21202663/the-ringer-nba-redraftables-series)
+        
         By combining draft results with a players career statistics, you can visualize whether a given draft pick was a good choice or a poor choice.
         """)
         st.image('./images/nba-redraftables.gif',use_column_width=True)
