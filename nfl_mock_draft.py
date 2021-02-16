72299bbeff7e3098f8d696ed284f4dbd68e1a9b3
Aaron Smith <aaroncolesmith@gmail.com>
2021-02-15 19:45:34 -0800

    adding_mock_draft

--- nfl_mock_draft.py
+++ nfl_mock_draft.py
@@ -16,6 +16,31 @@ def app():
     st.title('NFL Mock Draft Database')
     st.write('By scraping the results of multiple NFL Mock Drafts, can we start to see trends that will help understand how teams will draft?')
     df=load_data()
+
+    d1=pd.DataFrame()
+    d2=pd.DataFrame()
+    for i in df.player.unique():
+      d1 = pd.concat([d1, df.loc[df.player == i].iloc[0:5]])
+      d2 = pd.concat([d2, df.loc[df.player == i].iloc[5:10]])
+
+    d3=pd.merge(d1.groupby(['player']).agg({'pick':'mean'}).reset_index(), d2.groupby(['player']).agg({'pick':'mean'}).reset_index(), how='left', suffixes=('_0','_1'), left_on='player', right_on='player')
+    d3['chg'] = d3['pick_0'] - d3['pick_1']
+    d3['pct_chg']=d3['chg'] / d3['pick_1']
+    d3=d3.sort_values('chg',ascending=True)
+
+
+    col1, col2 = st.beta_columns(2)
+    col1.success("### Players Rising")
+    for i, r in d3.head(5).iterrows():
+        col1.write(r['player'] + ' | Avg. Pick changed ' + str(round(r['chg'],2)) +' picks')
+    # col1.write(d3.head(5))
+
+
+    col2.warning("### Players Falling")
+    for i, r in d3.tail(5).iterrows():
+        col2.write(r['player'] + ' | Avg. Pick changed ' + str(round(r['chg'],2)) +' picks')
+
+
     option = st.radio('View all or most recent mock drafts?',('All','Most Recent'))
     if option == 'All':
         d2 = df
