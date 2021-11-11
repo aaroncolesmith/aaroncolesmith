b113025ee3f508dc5430d9290f524e2566f123ba
Aaron Smith <32134610+aaroncolesmith@users.noreply.github.com>
2021-11-10 18:26:39 -0800

    scattergl

--- stock_prediction.py
+++ stock_prediction.py
@@ -8,12 +8,12 @@ import random
 
 def app():
     st.title('Stock Predictions')
-    stock_list = ['SPY','TSLA','AAPL','NKE','GME']
-    initial_stock = random.choice(stock_list)
+    # stock_list = ['SPY','TSLA','AAPL','NKE','GME']
+    # initial_stock = random.choice(stock_list)
 
     with st.form("input_form"):
         st.write("Pick a stock and see a prediction")
-        ticker = st.text_input("Stock Ticker", value=initial_stock)
+        ticker = st.text_input("Stock Ticker", value='AAPL')
         years_back = st.number_input("Years Back",min_value=.5, max_value=10.0, value=2.5, step=0.25)
         years_fwd = st.number_input("Years to Predict",min_value=.5, max_value=10.0, value=1.0, step=0.25)
         submit_button = st.form_submit_button(label='Submit')
@@ -40,19 +40,19 @@ def app():
         d2=d2[d2.ds.dt.dayofweek < 5]
 
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x=d2.ds,
+        fig.add_trace(go.Scattergl(x=d2.ds,
                                 y=d2.yhat_lower,
                                 opacity=.3,
                                 name='Low Threshold',
                                 line_color='rgba(75, 0, 130, .33)'))
-        fig.add_trace(go.Scatter(x=d2.ds,
+        fig.add_trace(go.Scattergl(x=d2.ds,
                                 y=d2.yhat_upper,
                                 fill='tonexty',
                                 opacity=.3,
                                 name='High Threshold',
                                 fillcolor='rgba(75, 0, 130, .25)',
                                 line_color='rgba(75, 0, 130, .33)'))
-        fig.add_trace(go.Scatter(x=d2.ds,
+        fig.add_trace(go.Scattergl(x=d2.ds,
                                 y=d2.y,
                                 name='Actual Stock Price',
                                 mode='markers',
@@ -61,7 +61,7 @@ def app():
                                             opacity=.5,
                                             line=dict(width=1,
                                                         color='#1320B2'))))
-        fig.add_trace(go.Scatter(x=d2.ds,
+        fig.add_trace(go.Scattergl(x=d2.ds,
                                 y=d2.yhat,
                                 name='Predicted Stock Price',
                                 mode='lines',
