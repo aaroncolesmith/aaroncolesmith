08f83261ecf224b52bd67e219e32b69b007487f1
aaron <aaroncolesmith@gmail.com>
2022-07-01 09:52:54 -0700

    fix spacing issues

--- pages/2_📚_Experience.py
+++ pages/2_📚_Experience.py
@@ -27,30 +27,24 @@ def app():
 
     ### [SafeRide Health](https://saferidehealth.com)
     #### Product Manager | 2020
-    - Built the technology platform to enable end-to-end patient transportation to care, supporting a number of different
-    use cases supporting Health Plans, Health Networks, NEMT Providers & ridesharing solutions
+    - Built the technology platform to enable end-to-end patient transportation to care, supporting a number of different use cases supporting Health Plans, Health Networks, NEMT Providers & ridesharing solutions
     - Automated existing Operations’ processes to enable the organization to scale without having to increase team size
     - Introduced software development processes to enable a much more collaborative product / engineering culture
 
     ---
     ### [Olive](https://oliveai.com)
     #### Product Manager - AI & Process Automation Platform | 2017 - 2020
-    - Built the software platform used to build and train our artificial intelligence solution, Olive, which allows an
-    automation engineer to utilize a wide range RPA functionality and integrate with state of the art technology like CV,
-    OCR and machine intelligence
-    - Developed from the ground up a state of the art process mining / intelligence product to greatly reduce the time to
-    analyze and document processes prior to building out automations
+    - Built the software platform used to build and train our artificial intelligence solution, Olive, which allows an automation engineer to utilize a wide range RPA functionality and integrate with state of the art technology like CV, OCR and machine intelligence
+    - Developed from the ground up a state of the art process mining / intelligence product to greatly reduce the time to analyze and document processes prior to building out automations
     - Increased automation build velocity by 500% by eliciting user feedback to implement usability enhancements
 
     #### Director - Data & Analytics | 2016 - 2018
-    - Built a data analytics solution to analyze our data across multiple data sources to provide the entire company with
-    actionable intelligence and insights to influence product and business strategy
+    - Built a data analytics solution to analyze our data across multiple data sources to provide the entire company with actionable intelligence and insights to influence product and business strategy
     - Shifted the company focus to make more data-driven decisions using the metrics and tools recently created
 
     #### Product Manager - Patient Registration & Internal Tools | 2015 - 2016
     - Developed the next-gen platform for our flagship product with a mindset of increasing scalability and engagement
-    - Collaborated cross-functionally with leadership to ensure the internal tools developed would support all necessary
-    job functions to allow CrossChx to grow at the rate it needed to meet investors’ demands
+    - Collaborated cross-functionally with leadership to ensure the internal tools developed would support all necessary job functions to allow CrossChx to grow at the rate it needed to meet investors’ demands
 
     ---
     ### [Cardinal Solutions Group](http://www.cardinalsolutions.com/)
@@ -65,8 +59,7 @@ def app():
     #### Consultant / Demand Planning Team Lead | 2011 - 2014
     - Analyzed, designed, and tested new functionality for DLA’s ERP solution which included enhancements to SAP (ECC 6.0) and JDA/Manugistics solutions as well as interfaces to Business Intelligence and Reporting systems
     - Managed the Demand Planning team to ensure all resources had a sufficient amount of work assigned
-    - As the JDA Demand Planning subject matter expert, provided guidance on all new features being built to assess
-    the potential impact to downstream data interfaces
+    - As the JDA Demand Planning subject matter expert, provided guidance on all new features being built to assess the potential impact to downstream data interfaces
     - Worked with key client contacts to maintain our current relationship while also working with our delivery leads to
     explore and expand our business relationship
     """)
