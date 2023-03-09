8de208bf64bccba20466964913fded52b18b146e
aaron <aaroncolesmith@gmail.com>
2023-03-08 20:40:10 -0800

    better version

--- pages/13_Betting_Scenarios.py
+++ pages/13_Betting_Scenarios.py
@@ -3,6 +3,9 @@ import streamlit as st
 import numpy as np
 import plotly_express as px
 import plotly.io as pio
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import datetime
 pio.templates.default = "simple_white"
 
 @st.cache(suppress_st_warning=True)
@@ -35,6 +38,18 @@ def update_df(df):
 
     for col in ['ml_away','ml_home','draw','total_over_money','ml_away_p','ml_home_p']:
         df[col]=pd.to_numeric(df[col])
+
+    # Convert datetime column to pandas datetime object
+    df['game_time'] = pd.to_datetime(df['start_time'])
+
+    # Convert to desired timezone (PT)
+    df['game_time'] = df['game_time'].dt.tz_convert('US/Pacific')
+
+    # Format datetime column as string in desired format
+    df['game_time'] = df['game_time'].dt.strftime('%Y-%m-%d %I:%M%p (%Z)')
+
+    df['game_title'] = pd.to_datetime(df['start_time']).dt.strftime('%m/%d') + ' - ' + df['away_team'] +' @ ' + df['home_team']
+
 
     return df
 
@@ -80,9 +95,22 @@ def app():
     d3['ml_home_change']=d3.groupby('id')['ml_home_p'].apply(lambda x: x.diff() / x.shift().abs())
     d3['ml_away_change']=d3.groupby('id')['ml_away_p'].apply(lambda x: x.diff() / x.shift().abs())
 
-    st.markdown('Home Teams in Upcoming Games Meeting Pct Change Threshold')
-    # pct_chg_threshold=.02
+    df_todays_games = pd.merge(d3, d3.groupby(["id"])["date_scraped"].max(), on=["id", "date_scraped"])[pd.to_datetime(d3["start_time"]).dt.date == datetime.date.today()].sort_values("start_time", ascending=True).reset_index(drop=True)[['id','game_time','home_team','away_team','ml_home_p','ml_away_p','ml_home_change','ml_away_change']]
 
+    for col in ['ml_home_p','ml_away_p']:
+        df_todays_games[col]=df_todays_games[col].apply(lambda x: '{:.2%}'.format(x))
+
+    for col in ['ml_home_change','ml_away_change']:
+        df_todays_games[col]=df_todays_games[col].apply(lambda x: '{:+.2%}'.format(x))
+
+    df_todays_games.columns=['ID','Game Time','Home','Away','Home Probability','Away Probability','Home Probability % Change','Away Probability % Change']
+
+    st.write('Today\'s Games')
+    st.dataframe(df_todays_games)
+
+
+
+    st.markdown('Home Teams in Upcoming Games Meeting Pct Change Threshold')
     pct_chg_threshold = st.number_input('Pct Change Threshold',value=.05)
     st.dataframe(d3.query("status == 'scheduled' & ml_home_change > @pct_chg_threshold")[['id','start_time','league_name','home_team','away_team','ml_home_p','ml_home_change']].sort_values('start_time',ascending=True))
     initial_game_id=d3.query("status == 'scheduled' & ml_home_change > @pct_chg_threshold")[['id','start_time','league_name','home_team','away_team','ml_home_p','ml_home_change']].sort_values('start_time',ascending=True).head(1)['id'].min()
@@ -91,9 +119,206 @@ def app():
     game_id = int(game_id)
 
 
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig.add_trace(
+        go.Scatter(
+            x=df.query('id==@game_id').date_scraped,
+            y=df.query('id==@game_id').ml_away,
+            mode="markers+lines",
+            name=df.query('id==@game_id')['away_team'].min() + ' Money Line (Away)',
+            marker=dict(
+                color="SpringGreen", size=10, line=dict(color="DarkSlateGrey", width=2),opacity=.75
+            ),
+            line = dict(width=4, dash='dash'),
+            opacity=0.6,
+            customdata=df.query('id==@game_id'),
+            hovertemplate=df.query('id==@game_id')['away_team'].min() + ' Money Line %{y} <br>Status: %{customdata[1]}<extra></extra>',
+
+        ),
+        secondary_y=False,
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df.query('id==@game_id').date_scraped,
+            y=df.query('id==@game_id').ml_away_p,
+            mode="lines+markers",
+            name=df.query('id==@game_id')['away_team'].min() + ' Probability (Away)',
+            marker=dict(
+                color="SpringGreen", size=10, line=dict(color="DarkSlateGrey", width=2),opacity=.75
+            ),
+            # marker_symbol='diamond',
+            line = dict(width=4),
+            opacity=0.6,
+            hovertemplate=df.query('id==@game_id')['away_team'].min() + ' Probability %{y}<extra></extra>',
+        ),
+        secondary_y=True,
+    )
+
+
+    fig.add_trace(
+        go.Scatter(
+            x=df.query('id==@game_id').date_scraped,
+            y=df.query('id==@game_id').ml_home,
+            mode="markers+lines",
+            name=df.query('id==@game_id')['home_team'].min() + ' Money Line (Home)',
+            marker=dict(
+                color="SpringGreen", size=10, line=dict(color="DarkSlateGrey", width=2),opacity=.75
+            ),
+            line = dict(width=4, dash='dash'),
+            opacity=0.6,
+            hovertemplate=df.query('id==@game_id')['home_team'].min() + ' Money Line %{y}<extra></extra>',
+        ),
+        secondary_y=False,
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df.query('id==@game_id').date_scraped,
+            y=df.query('id==@game_id').ml_home_p,
+            mode="lines+markers",
+            name=df.query('id==@game_id')['home_team'].min() + ' Probability (Home)',
+            marker=dict(
+                color="SpringGreen", size=10, line=dict(color="DarkSlateGrey", width=2),opacity=.75
+            ),
+            # marker_symbol='diamond',
+            line = dict(width=4),
+            opacity=0.6,
+            hovertemplate=df.query('id==@game_id')['home_team'].min() + ' Probability %{y}<extra></extra>',
+        ),
+        secondary_y=True,
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df.query('id==@game_id').date_scraped,
+            y=df.query('id==@game_id').draw,
+            mode="lines+markers",
+            name='Money Line - Tie',
+            marker=dict(
+                color="Blue", size=10, line=dict(color="DarkSlateGrey", width=2),opacity=.75
+            ),
+            # marker_symbol='diamond',
+            line = dict(width=4),
+            opacity=0.6,
+            hovertemplate='Money Line Tie - %{y}<extra></extra>'
+        ),
+        secondary_y=False,
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df.query('id==@game_id').date_scraped,
+            y=df.query('id==@game_id').total,
+            mode="lines+markers",
+            name='Over / Under',
+            marker=dict(
+                color="Blue", size=10, line=dict(color="DarkSlateGrey", width=2),opacity=.75
+            ),
+            # marker_symbol='diamond',
+            line = dict(width=4),
+            opacity=0.6,
+            hovertemplate='Over / Under - %{y}<extra></extra>'
+        ),
+        secondary_y=False,
+    )
+
+
+    if df.query('id==@game_id').total_over_money.max() > 0:
+        fig.add_trace(
+            go.Scatter(
+                x=df.query('id==@game_id').date_scraped,
+                y=df.query('id==@game_id').total_over_money,
+                mode="lines+markers",
+                name='Total Over Money',
+                marker=dict(
+                    color="Blue", size=10, line=dict(color="DarkSlateGrey", width=2),opacity=.75
+                ),
+                # marker_symbol='diamond',
+                line = dict(width=4),
+                opacity=0.6,
+                hovertemplate='Total Over Money - %{y}<extra></extra>'
+            ),
+            secondary_y=False,
+        )
+
+
+    if df.query('id==@game_id').ml_home_money.max() > 0:
+        fig.add_trace(
+            go.Scatter(
+                x=df.query('id==@game_id').date_scraped,
+                y=df.query('id==@game_id').ml_home_money,
+                mode="lines+markers",
+                name='ML Home Money',
+                marker=dict(
+                    color="Blue", size=10, line=dict(color="DarkSlateGrey", width=2),opacity=.75
+                ),
+                # marker_symbol='diamond',
+                line = dict(width=4),
+                opacity=0.6,
+                hovertemplate='ML Home Money - %{y}<extra></extra>'
+            ),
+            secondary_y=False,
+        )
+
+    if df.query('id==@game_id').ml_away_money.max() > 0:
+        fig.add_trace(
+            go.Scatter(
+                x=df.query('id==@game_id').date_scraped,
+                y=df.query('id==@game_id').ml_away_money,
+                mode="lines+markers",
+                name='ML Away Money',
+                marker=dict(
+                    color="Blue", size=10, line=dict(color="DarkSlateGrey", width=2),opacity=.75
+                ),
+                # marker_symbol='diamond',
+                line = dict(width=4),
+                opacity=0.6,
+                hovertemplate='ML Away Money - %{y}<extra></extra>'
+            ),
+            secondary_y=False,
+        )
+
+
+    fig.update_layout(hovermode="x", 
+                    # template="simple_white",
+                    height=600,
+                    title=df.query('id==@game_id')['game_title'].min(),
+                    legend=dict(yanchor="bottom", 
+                        y=-0.35, 
+                        xanchor="left", 
+                        x=0.0,
+                        orientation="h")
+                    )
+
+    fig.update_traces(textposition="top right", 
+                    mode="lines+markers", 
+                    line_shape="spline",
+                    opacity=.75,
+                        marker=dict(size=8,line=dict(width=1,color='DarkSlateGrey')),
+                        line = dict(width=4)
+                    )
+
+    fig.update_yaxes(title='Win Probability', tickformat=',.1%', secondary_y=True)
+    fig.update_yaxes(title='',secondary_y=False)
+
+    st.plotly_chart(fig,use_container_width=False)
+
+
+
+
+
+
+
+
+
     fig=px.scatter(df.query('id==@game_id'),
             x='date_scraped',
-            y=['ml_away','ml_home','draw','total_over_money','ml_away_p','ml_home_p'],
+            y=['ml_away','ml_home','draw','total',
+            'total_over_money','ml_home_money','ml_away_money',
+            
+            'ml_away_p','ml_home_p'],
             hover_data=['home_team','away_team','status']
     )
 
